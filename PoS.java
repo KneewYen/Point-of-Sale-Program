0cffb01565583b400114156bae237fdd56a28899
KneewYen <150541505+KneewYen@users.noreply.github.com>
2024-01-16 18:52:27 -0500

    PoS.java

--- PoS.java
+++ PoS.java
@@ -1,8 +1,4 @@
-// -------------------------------------------------------------
-// Assignment 4
-// Written by: Nguyen Le 40272922
-// For COMP 248 Section H – Fall 2023
-// -------------------------------------------------------------
+
 public class PoS {
 	//The PoS class manages sales and prepaid card information.
 	//It provides methods to compare sales, manage prepaid cards, and update sales data.
